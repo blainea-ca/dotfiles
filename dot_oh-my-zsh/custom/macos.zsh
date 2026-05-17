a6f565d63c8e21e318d142be0fbcc2d5a5658d96
Blaine Aldridge <blaine@blainea.ca>
2026-05-17 08:24:19 -0400

    Update custom/macos.zsh

--- dot_oh-my-zsh/custom/macos.zsh
+++ dot_oh-my-zsh/custom/macos.zsh
@@ -3,11 +3,24 @@ alias ls='eza -h'
 alias ll='eza -lh'
 alias la='eza -lha'
 
-alias gam="$HOME/bin/gamadv-xtd3/gam"
-alias gam-update="bash <(curl -s -S -L https://raw.githubusercontent.com/taers232c/GAMADV-XTD3/master/src/gam-install.sh) -l -p false"
 alias youtube-dl="yt-dlp"
 alias brewfile-update="brew bundle dump --describe -f --file ~/.Brewfile"
 alias brewfile-install="brew bundle install --file=~/.Brewfile"
+
+# GYB - Got Your Back (GYB) is a command line tool for backing up your Gmail messages to your local computer.
+# https://github.com/GAM-team/got-your-back
+alias gyb="~/bin/gyb/gyb"
+# Upgrade GYB
+alias upgrade_gyb="bash <(curl -s -S -L https://gyb-shortn.jaylee.us/gyb-install) -l"
+
+# GAM - A command line tool for Google Workspace admins to manage domain and user settings quickly and easily.
+# https://github.com/GAM-team/GAM
+alias gam="~/bin/gam7/gam"
+# Upgrade GAM
+alias upgrade_gam="bash <(curl -s -S -L https://git.io/gam-install) -l"
+
+# Podman Configuration
+export PODMAN_COMPOSE_WARNING_LOGS=false
 
 # Set default editor to nano
 export EDITOR=nano
